deb8a609ba5d094160a065f875398d0e645f3f96
SaveliiLapin <92331852+SaveliiLapin@users.noreply.github.com>
2022-12-18 17:53:18 +0400

    asdasdasd

--- Player.py
+++ Player.py
@@ -1,2 +1,5 @@
 class Player():
-    pass
+    def moveUp(self):
+        pass
+    def moveDown(self):
+        pass
---
+++ main.py
@@ -0,0 +1,10 @@
+import Player
+
+player = Player()
+
+.......
+
+if keys[K_Up]:
+    player.moveUp()
+elif keys[K_Down]:
+    player.moveDown()
